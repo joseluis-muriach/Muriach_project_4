6d4cf2eca85b925740617e2a2cab6d6cc6b2e9ef
Jose Luis Muriach <alumur497@ieselcaminas.org>
2023-09-19 17:43:01 +0200

    Exercise 2 completed

---
+++ app/src/main/java/com/example/muriach_proyecto_5/ui/theme/project5/Exercise2.kt
@@ -0,0 +1,97 @@
+package com.example.muriach_proyecto_5.ui.theme.project5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class Exercise2: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Cover()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Cover() {
+    Text(
+        text = "Welcome to: Perimeter of a square",
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            fontSize = 20.sp),
+        color = Color.Blue,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 20.dp),
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        var inputFirstNumber by remember { mutableStateOf("") }
+        val numberOfSides by remember { mutableStateOf("4") }
+        var resultOfTwoNUmbers by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = inputFirstNumber,
+            onValueChange = { inputFirstNumber = it },
+            label = {
+                Text("Enter the measure of the square")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            singleLine = true
+        )
+
+        Button(
+            onClick = {
+                resultOfTwoNUmbers = ((inputFirstNumber.toInt() *
+                                numberOfSides.toInt()).toString())
+            },
+            modifier = Modifier.padding(10.dp)){
+
+            Text(text = "Calculate")
+        }
+
+        Text(text = "The perimeter of the square is: ",
+            style = TextStyle(fontSize = 20.sp),
+            modifier = Modifier.padding(horizontal = 10.dp))
+        Text(
+            text = resultOfTwoNUmbers,
+            modifier = Modifier.padding(10.dp),
+            style = TextStyle(
+                fontSize = 20.sp)
+        )
+    }
+}
+
+
